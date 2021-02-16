60747a0597ae22f5fb967ee0ebefa710d9b6a027
lee.wp14 <lee.wp14@gmail.com>
2021-02-16 13:05:35 +0800

    trlte-common: remove Snap include entirely

--- trlte.mk
+++ trlte.mk
@@ -92,8 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.apq8084 \
-    libxml2 \
-    snap
+    libxml2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
